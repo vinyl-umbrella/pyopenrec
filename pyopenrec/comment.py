f6d0f6c4a7e9563ca7a599e01be702732689ee7d
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-12-18 18:51:40 +0900

    rewrite v2

---
+++ pyopenrec/comment.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+
+from .utils import http
+from .config import EXTERNAL_API, AUTHRIZED_API
+
+
+def get_comment(vid: str, from_created_at: str, limit=100) -> dict:
+    """
+    Get comments of live stream.
+
+    param
+    -----
+    vid: video id
+    from_created_at: ISO8601 format datetime (e.g. 2021-12-18T17:48:33+09:00)
+    limit: number of comments. max 300
+    """
+    url = EXTERNAL_API + "/movies/{}/chats".format(vid)
+    params = {
+        "from_created_at": from_created_at,
+        "is_including_system_message": "false",
+        "limit": limit
+    }
+    return http.request("GET", url, params)
+
+
+def get_recent_comment(vid: str, limit=100) -> dict:
+    """
+    Get recent comments of live stream.
+
+    param
+    -----
+    vid: video id
+    limit: number of comments. max 300
+    """
+    now = datetime.now().astimezone().isoformat(timespec="seconds")
+    url = EXTERNAL_API + "/movies/{}/chats".format(vid)
+
+    params = {
+        "to_created_at": now,
+        "is_including_system_message": "false",
+        "limit": limit
+    }
+    return http.request("GET", url, params)
+
+
+def get_vod_comment(vid: str):
+    """
+    Get comments of vod
+
+    param
+    -----
+    vid: video id
+    """
+    url = EXTERNAL_API + "/movies/{}/comments".format(vid)
+    return http.request("GET", url)
+
+
+def post_comment(vid: str, message: str, credentials):
+    """
+    post a comment to live stream
+
+    param
+    -----
+    vid: video id
+    message: message which you want to post
+    credentials: login data
+    """
+    url = AUTHRIZED_API + "/movies/{}/chats".format(vid)
+    params = {
+        "message": message,
+        "quality_type": 0,
+        "league_key": "",
+        "to_user_id": "",
+        "consented_chat_terms": "false"
+    }
+
+    return http.request("POST", url, params, credentials)
+
+
+def post_vod_comment(vid: str, message: str, credentials):
+    """
+    post a comment to vod
+
+    param
+    -----
+    vid: video id
+    message: message which you want to post
+    credentials: login data
+    """
+    url = AUTHRIZED_API + "/movies/{}/comments".format(vid)
+    params = {
+        "message": message
+    }
+    return http.request("POST", url, params, credentials)
+
+
+def reply_vod_comment(vid: str, comment_id: int, message: str, credentials):
+    url = AUTHRIZED_API + "/movies/{}/comments/{}/replies".format(vid, str(comment_id))
+    params = {
+        "message": message,
+        "consented_comment_terms": "true"
+    }
+    return http.request("POST", url, params, credentials)
--- sample/sample.py
+++ sample/sample.py
@@ -1,7 +1,26 @@
 import json
 import pyopenrec
 
+
+# # login
+# credentials = pyopenrec.login(email="YOUR_EMAIL", password="YOUR_PASSWORD")
+# print(credentials["data"])
+
+# # video
+# j = pyopenrec.get_stream_list()
+# j = pyopenrec.get_vod_list(page=2)
+# j = pyopenrec.get_movie_list(sort="-total_yells")
 j = pyopenrec.get_video_info("1o8q43q3yzk")
+# j = pyopenrec.get_video_detail("1o8q43q3yzk", credentials['data'])
+
+# # comment
+# j = pyopenrec.get_comment("n9ze3m2w184", "2021-12-18T17:30:33+09:00")
+# j = pyopenrec.get_recent_comment("n9ze3m2w184")
+# j = pyopenrec.get_vod_comment("e2zw69jmw8o")
+# j = pyopenrec.post_comment("kdr7x6y5w8j", "test", credentials["data"])
+# j = pyopenrec.post_vod_comment("n9ze3n30184", "test", credentials["data"])
+# j = pyopenrec.reply_vod_comment("e2zw69jmw8o", 140103, "test", credentials["data"])
+
 
 with open("test.json", "w") as f:
     json.dump(j, f, indent=2, ensure_ascii=False)
