8b91be21a17b9acc39eec714b14bed3a2e81743a
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-12-22 16:56:29 +0900

    update sample for 0.0.2

--- sample/get_all_comments.py
+++ sample/get_all_comments.py
@@ -1,14 +1,14 @@
 from datetime import datetime
-import pyopenrec
+from pyopenrec.comment import Comment
 import time
 
 
 last_id = 0
-dt = datetime(2015, 1, 1, 0, 0, 0)
+dt = datetime(2021, 12, 22, 15, 0, 0)
 vid = input("input video id: ")
 
 while True:
-    comments = pyopenrec.get_comment(vid, dt, 5)
+    comments = Comment.get_comment(vid, dt, 5)
     # if got all comments, exit
     if comments["data"][-1]["id"] == last_id:
         break
@@ -16,8 +16,8 @@ while True:
     for comment in comments["data"]:
         if comment["id"] <= last_id:
             continue
-        print(comment["id"], comment["posted_at"],
-              comment["user"]["id"], comment["user"]["nickname"], comment["message"])
+        print(comment["posted_at"], comment["user"]["nickname"],
+              comment["user"]["id"], comment["message"])
 
     last_id = comments["data"][-1]["id"]
     dt = datetime.fromisoformat(comments["data"][-1]["posted_at"])
