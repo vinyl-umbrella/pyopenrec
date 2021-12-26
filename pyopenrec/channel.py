f576ebfbc9ba771c3a5596492fc391ea91e92640
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-12-27 01:34:58 +0900

    get follow and follower list

--- pyopenrec/channel.py
+++ pyopenrec/channel.py
@@ -9,6 +9,7 @@ class Channel:
     - Get subscription info.
     - Get whether the user is moving or not.
     """
+    _credentials = {}
     _proxy = {}
 
     def channel_rank(self, period, date=None, page=1) -> dict:
@@ -50,6 +51,47 @@ class Channel:
         """
         url = EXTERNAL_API + "/subs-channels/{}".format(user_id)
         return http.request("GET", url, proxy=self._proxy)
+
+    def _get_follow(self, user_id: str, is_follow: int, page: int):
+        """
+        param
+        -----
+        user_id: user id
+        is_follow:
+            0: follower
+            1: follow
+        page: page number
+        """
+        url = "https://www.openrec.tv/api/v3/get_follow_list"
+        recxuser_id = self.channel_info(user_id)["data"]["recxuser_id"]
+        params = {
+            "recxuser_id": recxuser_id,
+            "is_follow": is_follow,
+            "page_number": page
+        }
+        return http.request("GET", url, params, credentials=self._credentials, proxy=self._proxy)
+
+    def get_follow(self, user_id: str, page=1):
+        """
+        get `user_id`'s follow list
+
+        param
+        -----
+        user_id: user id
+        page: page number
+        """
+        return self._get_follow(user_id, 1, page)
+
+    def get_follower(self, user_id: str, page=1):
+        """
+        get `user_id`'s followers list
+
+        param
+        -----
+        user_id: user id
+        page: page number
+        """
+        return self._get_follow(user_id, 0, page)
 
     def is_streaming(self, user_id: str) -> bool:
         """
