9a0721136b05e7fbde45f17509265430dfa3d6f9
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2021-12-26 16:40:47 +0900

    update sample/sample.py

--- sample/sample.py
+++ sample/sample.py
@@ -1,14 +1,17 @@
-import json
+# import json
 import pyopenrec
 
 
 # login with email
-# user = pyopenrec.Openrec(email="YOUR_EMAIL", password="YOUR_PASSWORD")
-# j = user.timeline(2)
+user = pyopenrec.Openrec(email="YOUR_EMAIL", password="YOUR_PASSWORD")
+# with open("./sample/secret.json", "r") as f:
+#     cred = json.load(f)
 
-# without login
-user = pyopenrec.Openrec()
-j = user.post_capture_reaction("l9nk1xwegd1", "kawaii")
+# user = pyopenrec.Openrec(credentials=cred)
+print("Login as {} ({})".format(user.name, user.id))
 
-with open("test.json", "w") as f:
-    json.dump(j, f, indent=2, ensure_ascii=False)
+j = user.post_comment("n9ze3m2w184", "てすと")
+if j["status"] == 200:
+    print(j["data"])
+else:
+    print("Failed")
