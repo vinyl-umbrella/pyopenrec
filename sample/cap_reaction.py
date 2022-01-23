d4b391c1fba039b8b3bad0c1a9a05cb72e4bac94
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2022-01-23 15:10:19 +0900

    update tools

--- sample/cap_reaction.py
+++ sample/cap_reaction.py
@@ -1,10 +1,32 @@
-# increase capture reaction
+import subprocess
+import time
+from multiprocessing import Pool
+
 import pyopenrec
 
+
+tor_proxy = {
+    "http": "socks5://127.0.0.1:9050",
+    "https": "socks5://127.0.0.1:9050",
+}
 cap_id = input("input capture id: ")
 
-for _ in range(3):
-    # make no login user
-    user = pyopenrec.Openrec()
+
+def post_reaction(cap_id: str):
+    # make no login user through tor
+    user = pyopenrec.Openrec(proxy=tor_proxy)
+    print("logined")
     # post reaction
-    user.post_capture_reaction(cap_id, "kawaii")
+    j = user.post_capture_reaction(cap_id, "kawaii")
+    print(j["status"])
+
+
+while True:
+    subprocess.run(["sudo", "-p", "database", "service", "tor", "restart"])
+    time.sleep(5)
+
+    p = Pool(4)
+    for _ in range(20):
+        p.apply_async(post_reaction, args=(cap_id,))
+
+    time.sleep(60)
--- sample/rentou.py
+++ sample/rentou.py
@@ -1,13 +1,32 @@
 # 連投 without login
-from copy import deepcopy
+import subprocess
+import time
+from multiprocessing import Pool
 
 import pyopenrec
 
+tor_proxy = {
+    "http": "socks5://127.0.0.1:9050",
+    "https": "socks5://127.0.0.1:9050",
+}
 vid = input("input stream id: ")
 
-for _ in range(3):
+
+def post_template_comment(vid: str):
     # make no login user
-    user = pyopenrec.Openrec()
-    copied_user = deepcopy(user)
+    user = pyopenrec.Openrec(proxy=tor_proxy)
+    print("logined")
     # post template message
-    copied_user.post_template_comment(vid, 1)
+    j = user.post_template_comment(vid, 0)
+    print(j["status"])
+
+
+while True:
+    subprocess.run(["sudo", "-p", "database", "service", "tor", "restart"])
+    time.sleep(5)
+
+    p = Pool(5)
+    for _ in range(10):
+        p.apply_async(post_template_comment, args=(vid,))
+
+    time.sleep(60)
