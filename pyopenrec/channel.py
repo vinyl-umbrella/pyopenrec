5e1c00916e7b30efc80929a1c63fc0c1a2b51666
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2022-05-09 16:27:24 +0900

    use api v5 for getting follow/follower

--- pyopenrec/channel.py
+++ pyopenrec/channel.py
@@ -10,7 +10,7 @@ class Channel:
     - Get `user_id`'s  coming ups, vods and current stream list.
     - get `user_id`'s follow list
     - get `user_id`'s follower list
-    - Get whether the user is moving or not.
+    - Get whether the user is streaming or not.
     """
     _credentials = None
     _proxy = {}
@@ -79,35 +79,22 @@ class Channel:
         }
         return http.request("GET", url, params=params, proxy=self._proxy)
 
-    def _get_follow(self, user_id: str, is_follow: int, page: int) -> http.Response:
-        """
-        param
-        -----
-        user_id: user id
-        is_follow:
-            0: follower
-            1: follow
-        page: page number
-        """
-        url = "https://www.openrec.tv/api/v3/get_follow_list"
-        recxuser_id = self.channel_info(user_id).data["recxuser_id"]
-        params = {
-            "recxuser_id": recxuser_id,
-            "is_follow": is_follow,
-            "page_number": page
-        }
-        return http.request("GET", url, params, credentials=self._credentials, proxy=self._proxy)
-
-    def get_follow(self, user_id: str, page=1) -> http.Response:
+    def get_follow(self, user_id: str, sort="followed_at", page=1) -> http.Response:
         """
         get `user_id`'s follow list
 
         param
         -----
         user_id: user id
+        sort: followed_at || movie_count || movie_published_at
         page: page number
         """
-        return self._get_follow(user_id, 1, page)
+        url = f"{EXTERNAL_API}/users/{user_id}/follows"
+        params = {
+            "sort": sort,
+            "page": page
+        }
+        return http.request("GET", url, params, proxy=self._proxy)
 
     def get_follower(self, user_id: str, page=1) -> http.Response:
         """
@@ -118,7 +105,11 @@ class Channel:
         user_id: user id
         page: page number
         """
-        return self._get_follow(user_id, 0, page)
+        url = f"{EXTERNAL_API}/users/{user_id}/followers"
+        params = {
+            "page": page
+        }
+        return http.request("GET", url, params, proxy=self._proxy)
 
     def is_streaming(self, user_id: str) -> bool:
         """
