3f192e0194a6fab01bcd9e854d2db6f24088eabf
vinyl-umbrella <61788251+vinyl-umbrella@users.noreply.github.com>
2023-07-01 19:30:47 +0900

    add test

--- tests/main.py
+++ tests/main.py
@@ -1,26 +1,18 @@
 import unittest
 
 import test_capture
-import test_channel
-import test_chat
-import test_comment
-import test_playlist
+import test_user
 import test_video
-import test_yell
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(test_capture.TestCapture))
-    suite.addTest(unittest.makeSuite(test_channel.TestChannel))
-    suite.addTest(unittest.makeSuite(test_chat.TestChat))
-    suite.addTest(unittest.makeSuite(test_comment.TestComment))
-    suite.addTest(unittest.makeSuite(test_playlist.TestPlaylist))
     suite.addTest(unittest.makeSuite(test_video.TestVideo))
-    suite.addTest(unittest.makeSuite(test_yell.TestYell))
+    suite.addTest(unittest.makeSuite(test_user.TestUser))
     return suite
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mySuite = suite()
     unittest.TextTestRunner().run(mySuite)
